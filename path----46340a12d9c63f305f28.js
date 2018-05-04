bdfdc940a0eb9f71246dd3629aee4628fff69aef
johntzan <john.tzanida@gmail.com>
2018-05-04 02:11:49 -0400

    Updates

---
+++ path----46340a12d9c63f305f28.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{99:function(e,o){e.exports={data:{site:{siteMetadata:{title:"John Tzanidakis",description:"Mobile & Web Developer",url:"https://www.johntzan.com",keywords:"mobile, full stack, developer, engineer, android, portfolio, personal website"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----46340a12d9c63f305f28.js.map
